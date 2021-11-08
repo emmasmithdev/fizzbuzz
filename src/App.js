c59225f40c92ee6961ec7887f121059bf106d665
Emma <emmagray183@gmail.com>
2021-11-08 22:34:05 +0000

    Uses state variables and useEffect hook to display current number and correct answer.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from "react";
 import './App.css';
 
 function App() {
+
+  const [number, setNumber] = useState(1);
+  const [output, setOutput] = useState(0);
+
+  useEffect( () => {
+    console.log("effect triggered");
+    if (number % 3 === 0 && number % 5 === 0) {
+      setOutput("fizzbuz")
+    } else if (number % 3 === 0) {
+      setOutput("fizz")
+    } else if (number % 5 === 0) {
+      setOutput("buzz")
+    } else {
+      setOutput(number);
+    }
+  }, [number])
+
+  const handleInc = () => {
+    setNumber(number + 1);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button value={number} onClick={handleInc}>+</button>
+      <h2>The current number is {number}</h2>
+      <h2>The answer is {output}</h2>
     </div>
   );
 }
